ae76df8e57876e20f71e2a02ba00aeb0b8159bc7
Leo Guerrero <lguerr8@lsu.edu>
2024-09-16 09:24:44 -0500

    2nd commit

--- Assignment 2/buffer.py
+++ Assignment 2/buffer.py
@@ -1,7 +1,7 @@
 import arcpy
 arcpy.analysis.Buffer(
     in_features="cities.shp",
-    out_feature_class="./cities_buffer_1.shp",
+    out_feature_class="./cities_buffer.shp",
     buffer_distance_or_field="100 Meters",
     line_side="FULL",
     line_end_type="ROUND",
